38578f0bc4ff92aebd71e1ae2e1e055552d0eec0
Daniel <106691693+DMF1989@users.noreply.github.com>
2023-12-06 12:03:02 -0500

    Add files via upload
    Una aplicación que nos muestra los nombres ordinales y cardinales en español de los números que insertemos

--- app.js
+++ app.js
@@ -1,252 +1,151 @@
-let label = document.querySelector('label');
-let answer = document.getElementById("result");
-let text = document.getElementById("thousandsText");
-let base = document.getElementById("base");
-let romanField = document.getElementById("romanNumber");
-var nume = document.querySelector("form > span");
-var boter = document.querySelector("button");
-const container = document.getElementById('only');
-var hourTime = document.querySelector("span:first-of-type");
-var dateHTML = document.getElementById('date');
-var oneLine;
-var overline;
-var twoLine
-var doubleline;
-var threeLine
-var tripleline;
+    var quote = '';
+    var s;
+    var interval;
 
-let romanNumerals = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000
-};
+    var miCampo = document.getElementById('enter');
+    var span = document.getElementsByTagName("span");
+    var tit = document.querySelectorAll("h3");
+    var label = document.getElementsByTagName("label")
 
-let regexDecimal = /^[0-9]*$/i;
-let regexRoman = /^[IVXLCDM]*$/i;
+    var a = document.getElementById("a");
+    var chk = document.getElementById("g");
+    var skc = document.getElementById('"');
+    var apocope = document.getElementById("apocope");
 
-base.onkeydown = function(e) {
-  const validKeys = ["Backspace", "Delete", "ArrowLeft", "ArrowRight"];
-  if (!regexRoman.test(e.key) && !validKeys.includes(e.key)) {e.preventDefault()}
-}
-var id;
-base.onkeyup = function(e) {
-  id = '';
-  romanToDecimal(e.target.innerText);
-  del()
-}
+    a.addEventListener("click",() => { submit() } )
+    
+    chk.addEventListener("click", change);
+    skc.addEventListener("click",() => {
+    if(skc.checked==true){quote = '"'; submit()} 
+    else {quote = ''; submit()}});
 
-var romanNumeral;
-// Uso de la función
-romanField.addEventListener('input', function(e) {
-  id = 'a'
-  const valor = e.target.value;
-    // Eliminar cualquier carácter no numérico y no romano
-    var num = e.target.value.replace(/[^0-9.ivxlcdm]/g, "");
-    // Si es un número decimal, formatearlo
-    if (/^[0-9.]+$/.test(num)) {
-        e.target.value = formatNumber(num);
-        romanNumeral = decimalToRoman((valor).replaceAll(' ',''));
-        base.innerText = '';
-    }
-    // Si es un número romano, dejarlo como está
-    else if (/^[ivxlcdm]+$/.test(num)) {
-        e.target.value = num;
-        romanToDecimal(valor);
-        doubleline = '';
-        overline = ''
-    }
-    // Si es una mezcla de números romanos y decimales, borrar la entrada
-    else if (!(regexRoman.test(valor) || regexDecimal.test(valor))) {
-        e.target.value = '';
+    function selOpt() {
+        var select = document.getElementById('mySelect').value;
+        if(select == 'lowercase'){s = 'l'; submit()};
+        if(select == 'capitalize'){s = 'c'; submit()};
+        if(select == 'uppercase'){s = 'u'; submit()};
+    };    
+    
+    function change(){if(chk.checked) {g = 'f'; apocope.checked=false; submit()} 
+    else {g = 'm'; submit()}
     };
-    if ((/\d/).test(valor)) {base.style.display = 'none'; changelanguaje()} else if (regexRoman.test(valor)) {base.style.display = 'flex'; changelanguaje()};
-    if (!romanField.value) {base.style.display = 'flex'; changelanguaje()};
-    del()
-})
 
-function formatNumber(n) {
-  // Convertir el número a una cadena y dividir la parte entera de los decimales
-  var parts = n.toString().split(" ");
-  // Reemplazar los números con una coma cada tres dígitos en la parte entera
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-  // Unir la parte entera y decimal de nuevo
-  return parts;
-}
+    apocope.addEventListener("click",() => {
+        if (apocope.checked) {change()}
+    })
 
-function del(){
-  if(!base.innerText && !romanField.value) {text.innerHTML = ''; answer.innerHTML = ''; overline = ''; 
-  removeElement()} 
-};
+    submit = () => {
 
-var correctorCalled;
-var romanToDecimal = function (e){
+    for (let h3 of tit) {
+        if (miCampo.value != 0) {h3.style.display = "block"} else {h3.style.display = "none"}
+    };  
 
-        var arabicNumber = 0;
-        let romanNumber = e.toUpperCase();
+    if (apocope.checked==true) {chk.checked=false};
+    let value = enter.value.replace(/[^0-9]/g, '');
 
-        if ((/IIII|XXXX|CCCC|MMMM/).test(romanNumber)) {corrector(0)};
-
-        if ((/VV|LL|DD/).test(romanNumber)) {corrector(1)};
-        
-        for (let i = 0; i < romanNumber.length; i++) {
-          const currentValue = Number(romanNumerals[romanNumber[i]]);
-          const nextValue = Number(romanNumerals[romanNumber[i + 1]]);
-          const secondNextValue = Number(romanNumerals[romanNumber[i + 2]]);
-          if (nextValue && nextValue > currentValue) {
-            arabicNumber -= currentValue;
-            if([5, 50, 500].includes(currentValue)) {corrector(2)};
-
-            for (let f = 0; f < 3; f++){
-            if(currentValue == 1*10**f && ![5*10**f, 10*10**f].includes(nextValue)) {
-              TypeOne(romanNumber[i], romanNumber[i + 1]);
-              corrector(oneError);
-              };
-            };
-
-            if (['V', 'L', 'D'].includes(romanNumber[i-1])) if(romanNumber[i-1] == romanNumber[i+1]) {corrector(3)};
-
-            if (['I', 'X', 'C'].includes(romanNumber[i])) if(romanNumber[i] == romanNumber[i+2]) {corrector(4)} else if (romanNumber[i-1] == romanNumber[i]) {corrector(5)};
-
-            if (currentValue < (nextValue, secondNextValue)) {corrector(6)}
-          } else {
-            arabicNumber += currentValue;
-          }
-        }
-        return arabicNumber;
-};
-
-function decimalToRoman(decimal) {
-  var roman = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
-  var ans = '';
-
-  var convert = function(value) {
-    var result = '';
-    for (var a in roman) {
-      while (value >= roman[a]) {
-        result += a;
-        value -= roman[a];
-      }
+    if(value == 7){pre = ' o '; span[2].style.display = 'inline'} 
+    else if([11, 12, 13].includes(Number(value))) {span[2].style.display = 'inline'}
+    else{pre = ' y ';span[2].style.display = 'none'}
+ 
+    span[0].innerHTML = quote + toOrdinal(value, g, s) + quote;
+    span[1].innerHTML = ' o ' + quote + alOrdinal(value - 11, g, s) + quote;
+    span[2].innerHTML = pre + quote + alOrdinal2(value - 7, g, s) + quote;
+    span[3].innerHTML = quote + cardinales(value) + quote;
+    
+    if(value == 0){[0, 3].forEach(i => span[i].innerHTML = '')};
+    if([11, 13].includes(Number(value))){span[1].style.display = "inline"}
+    else {span[1].style.display = "none"};
     }
-    return result;
-  };
 
-  if (decimal > 3999999999) {
-    var bilions = Math.floor(decimal / 1e9);
-    decimal %= 1e9;
-    tripleline = convert(bilions);
-  } else if (decimal < 4e9) {tripleline = ''}
-  else {tripleline = ''}
+    function changeCase(ordinal){
+        if (s == 'u') {return ordinal.toUpperCase()}
+        else if (s == 'c') {return capitalizeWords(ordinal)}
+        if (s == 'l') {return ordinal.toLowerCase()};
+        return ordinal
+    };
 
-  if (decimal > 3999999) {
-    var millions = Math.floor(decimal / 1e6);
-    decimal %= 1e6;
-    doubleline = convert(millions);
-  } else if (decimal < 4e6) {doubleline = ''}
-  else {doubleline = ''}
+    function capitalizeWords(str) {
+        let words = str.split(' ');
+        let result = [];
+        for (let i = 0; i < words.length; i++) {
+            if (words[i] !== 'y') {
+                result.push(words[i].charAt(0).toUpperCase() + words[i].slice(1));
+            } else {
+                result.push(words[i]);
+            }
+        }
+        return result.join(' ');
+    }
 
-  if (decimal > 3999) {
-    var thousands = Math.floor(decimal / 1e3);
-    decimal %= 1e3;
-    overline = convert(thousands);
-  } else if (decimal < 4000) {overline = ''}
-  else {overline = ''}
+    miCampo.addEventListener('keypress', function (evt) {
+        var key = evt.key;
+        if (key < '0' || key > '9')
+        {
+            evt.preventDefault();
+        }
+    });
 
-  ans += convert(decimal);
-  return ans;
-};
+    miCampo.addEventListener('input', formatValue);
 
-function result(){
-  removeElement();
+    function formatNumber(value) {
+        return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    }
+    
+    function formatValue(e) {
+        e.target.value = formatNumber(e.target.value.replace(/[^0-9]/g, ''));
+    }
+    
+    function changeValue(delta) {
+        var currentValue = miCampo.value === '' ? 0 : miCampo.value.replace(/ /g, '');
+        if (currentValue === 0 && delta < 0) {
+            return;
+        }
+        var newValue = new BigNumber(currentValue).plus(delta).toFixed();
+        if (newValue >= 0) {
+            miCampo.value = newValue == 0 ? '' : formatNumber(newValue);
+            submit();
+        }
+    }
 
-  if(overline) {oneLine = document.createElement("span");
-  oneLine.innerHTML = overline;
-  oneLine.style.cssText = 'text-decoration: overline; position: relative; padding-top: 4px; font-size: 20px; margin-left: 3px';
-  answer.parentNode.insertBefore(oneLine, answer)}
-
-  if(doubleline) {twoLine = document.createElement("span");
-  twoLine.innerHTML = doubleline;
-  twoLine.style.cssText = 'text-decoration: overline; border-top: 2px solid black; position: relative; padding-top: 4px; font-size: 20px; margin-left: 3px';
-  text.insertAdjacentElement('afterend', twoLine)}
-
-  if(tripleline) {threeLine = document.createElement("span");
-  threeLine.innerHTML = tripleline;
-  threeLine.style.cssText = 'text-decoration: overline; border-top: 2px solid black; position: relative; padding-top: 4px; font-size: 20px; margin-left: 3px';
-  threeLine.classList.add('line-before');
-  text.insertAdjacentElement('afterend', threeLine)}
-
-        if (regexRoman.test(romanField.value) || base.innerText) {answer.innerHTML = (Number(romanToDecimal(base.innerText))*1000 + Number(romanToDecimal(romanField.value))).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ') + '.'; if (answer.classList.contains('margintext')) {answer.classList.remove('margintext')}} 
-        else {answer.innerHTML = romanNumeral + '.';
-        answer.classList.add('margintext')};
-        if (!romanField.value && !base.innerText) {text.innerText = empty; answer.innerHTML = ''; setTimeout(function() {
-          text.innerText = '';
-      }, 3000)} else {text.innerText = response};
-};
-
-  const keys = Object.keys(romanNumerals);
-  const values = Object.values(romanNumerals);
-  for (let i = 0; i < 7; i++) {
-  const h2 = document.createElement('h2');
-  const color = i % 2 === 0 ? '#1E90FF' : '#00bcd4';
-  h2.innerHTML = `<span style="color:#32CD32">${keys[i]}</span> = <span style="color:${color}">${values[i]}</span>`;
-  var titleArray = ['ūnus','quinque','decem','quinquaginta','centum','Quingenti','Mille'];
-  h2.title = `${titleArray[i]}`
-  container.appendChild(h2);
-};
-
-corrector = (i) => {
-  correctorCalled = true;
-  removeElement();
-  index = i;
-  if (id == 'a') {romanField.value = romanField.value.slice(0, -1)}
-  else {cursor()};
-
-  text.innerText = invalid;
-  if (i == oneError) {answer.innerHTML = oneError} else {answer.innerHTML = invAns[i]};
-  setTimeout(function() {index = ''; answer.innerHTML = '', text.innerText = ''; }, 3000);
-};
-
-function cursor() {
-  var rango = window.getSelection().getRangeAt(0);
-  var posicion = rango.startOffset;
-  base.innerText = base.innerText.slice(0, -1)
-  if (posicion > base.innerText.length) {
-    posicion = base.innerText.length;
-};
-  rango.setStart(base.childNodes[0], posicion);
-  rango.setEnd(base.childNodes[0], posicion);
-  window.getSelection().removeAllRanges();
-  window.getSelection().addRange(rango);
+    function handleButton(buttonId, delta) {
+    var button = document.getElementById(buttonId);
+    button.addEventListener("mousedown", function(event) {
+      if(event.button == 2) return; 
+      changeValue(delta);
+      interval = setInterval(changeValue, 100, delta);
+    });
+    button.addEventListener("mouseup", function() {
+      clearInterval(interval);
+    });
 }
+      
+      handleButton("incrementButton", 1);
+      handleButton("decrementButton", -1);
+      
+      miCampo.addEventListener('paste', function (e) {
+        var textoPegado = (e.clipboardData || window.Clipboard).getData('text');
+        var textoFiltrado = textoPegado.replace(/\D/g,'');
+        if (textoPegado !== textoFiltrado) {
+            e.preventDefault();
+            document.execCommand('insertText', false, textoFiltrado);
+        }
+    });
 
-romanField.addEventListener('paste', paste);
-base.addEventListener('paste', paste);
 
-function paste(e) {
-  // Cancela la operación de pegado original
-  e.preventDefault();
-
-  // Obtiene el texto del portapapeles
-  var text = e.clipboardData.getData('text/plain').trim();
-
-  // Inserta el texto sin formato en el elemento editable
-  document.execCommand('insertText', false, text);
- if(regexRoman) {romanToDecimal(text)};
-if (correctorCalled) if (id == 'a') {romanToDecimal(romanField.value)}
-else {romanToDecimal(base.innerText)}
-}
-
-function removeElement() {
-  if (text.parentNode.contains(oneLine)) {
-    text.parentNode.removeChild(oneLine);
-  }
-  if (answer.parentNode.contains(twoLine)) {
-    answer.parentNode.removeChild(twoLine)
-  }
-  if (answer.parentNode.contains(threeLine)) {
-    answer.parentNode.removeChild(threeLine)
-  }
-}
+  
+    enter.addEventListener('input', function() {
+        const textWidth = getTextWidth(this.value);
+        this.style.width = (textWidth + 200) + 'px';
+      });
+      
+      function getTextWidth(text) {
+        const dummyElement = document.createElement('span');
+        dummyElement.style.visibility = 'hidden';
+        dummyElement.style.whiteSpace = 'nowrap';
+        dummyElement.textContent = text;
+        document.body.appendChild(dummyElement);
+        const width = dummyElement.getBoundingClientRect().width;
+        document.body.removeChild(dummyElement);
+        return width;
+      }
+      
